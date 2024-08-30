0fea2244ed5b08088c2af0dad580515729826dca
Ignacio <104097123+ignaciogiacotto@users.noreply.github.com>
2024-08-30 17:43:18 -0300

    creando componente Forbidden e interceptorToken

---
+++ src/app/components/forbidden403/forbidden403.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'orbidden403',
+  standalone: true,
+  imports: [],
+  templateUrl: './forbidden403.component.html'
+})
+export class Forbidden403Component {
+
+}
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,33 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  inject(Router).navigate(['/login'])
+  const service = inject(AuthService);
+  const router = inject(Router);
+  
+  if(service.isAuthenticated()){
+    if(isTokenExpired()){
+      service.logout();
+      router.navigate(['/login'])
+      return false;
+    }
+    if(!service.isAdministrator()){
+      router.navigate(['/forbidden'])
+      return false;
+    }
+    return true;
+  }
+  router.navigate(['/login']);
+
   return false;
 };
+
+const isTokenExpired = () => {
+  const service = inject(AuthService);
+  const token = service.token;
+  const payload = service.getPayload(token);
+  const exp = payload.exp;
+  const now = new Date().getTime()/1000;
+  return (now > exp) ? true : false;
+}
